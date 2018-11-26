b781c26ff9ca1c3275644f5a5e433276794e20a5
Carlos Valderrama <cvalderrama@tecnosoftware.com>
2018-11-26 18:56:07 -0300

    review deploy tomcat

--- src/main/java/com/empr/begin/services/DemoApplication.java
+++ src/main/java/com/empr/begin/services/DemoApplication.java
@@ -4,13 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		
-		
-		
+						
 		SpringApplication.run(DemoApplication.class, args);
 		
 	}
+	
+	 @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(DemoApplication.class);
+	  }
+	 
 }
